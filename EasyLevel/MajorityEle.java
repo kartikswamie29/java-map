990f082f1ac40916c35775f4469f0f467a0397a6
kartikswamie29 <kartikswamie29@gmail.com>
2025-12-30 12:40:34 +0530

    Create MajorityEle.java

---
+++ EasyLevel/MajorityEle.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int num : nums)
+        {
+            hm.put(num,hm.getOrDefault(num,0)+1);
+        }
+        int result=0;
+        int reqfreq;
+        for(int key : hm.keySet())
+        {
+            if(hm.get(key)>nums.length/2)
+            {
+                return key;
+            }
+        }
+        return -1;
+    }
+}
+
