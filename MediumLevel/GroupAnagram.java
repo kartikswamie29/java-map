9722aa2e030c329832e38e2f68e3b6240c68cf6d
kartikswamie29 <kartikswamie29@gmail.com>
2026-01-01 17:57:50 +0530

    Create GroupAnagram.java

---
+++ MediumLevel/GroupAnagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);                 // sort characters
+            String key = new String(ch);     // sorted word as key
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
